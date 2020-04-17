46a2630280ac48ff6d70178703d797d21a69da85
Kemici Cagnard <kemici.cagnard@outlook.com>
2020-04-17 10:29:43 +0200

    produit controller

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('stock')
+            ->add('photo')
+            ->add('Prix')
+            ->add('ContenuPaniers')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
